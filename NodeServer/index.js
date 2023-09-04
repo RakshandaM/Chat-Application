9fbaa9c0efa7c2e96f42e27ae253701f304392d1
Rakshanda.M <rakashandamadan@gmail.com>
2023-09-04 22:45:37 +0530

    Add CORS

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,20 +1,23 @@
 //Node server which will handle socket io connections.
 
 const io = require('socket.io')(8000) //Server (listen incoming event)
+const cors = require('cors'); // Require the 'cors' package
 
 const user = {};
 
+// Enable CORS for your server
+io.use(cors());
+
 io.on ('connection' , socket =>{            //Listen all the new user connection
-    socket.on ('new-user-joined' , name =>{ 
-        console.log ("New user" , Uname );      //user join
-        user[socket.id]=name;                 //When you get user call socket.om
+    socket.on ('new-user-joined' , name =>{       //user join
+        user[socket.id]=name;                 //When you get user call socket.on
         socket.broadcast.emit('user-joined' , name); //broadcast.emit (New user has joined the chat {excluding him})    
     });
 
     //Send Message
     socket.on('send' , message =>{
-        socket.broadcast.emit('receive', {message:message ,name: user[socket.id]})  //receive the message     
+        socket.broadcast.emit('receive', {message:message ,uname: user[socket.id]})  //receive the message     
     });
-
 })
+
 
