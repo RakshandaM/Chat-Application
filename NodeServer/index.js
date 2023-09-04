1d183a4a7f4a5709ade1b77cd32de402c1a89e01
Rakshanda.M <rakashandamadan@gmail.com>
2023-09-05 01:35:44 +0530

    Add client.js

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,22 +1,19 @@
 //Node server which will handle socket io connections.
 
 const io = require('socket.io')(8000) //Server (listen incoming event)
-const cors = require('cors'); // Require the 'cors' package
 
 const user = {};
 
-// Enable CORS for your server
-io.use(cors());
 
 io.on ('connection' , socket =>{            //Listen all the new user connection
-    socket.on ('new-user-joined' , name =>{       //user join
-        user[socket.id]=name;                 //When you get user call socket.on
-        socket.broadcast.emit('user-joined' , name); //broadcast.emit (New user has joined the chat {excluding him})    
+    socket.on ('new-user-joined' , name1 =>{       //user join
+        user[socket.id]=name1;                 //When you get user call socket.on
+        socket.broadcast.emit('user-joined' , name1); //broadcast.emit (New user has joined the chat {excluding him})    
     });
 
     //Send Message
     socket.on('send' , message =>{
-        socket.broadcast.emit('receive', {message:message ,uname: user[socket.id]})  //receive the message     
+        socket.broadcast.emit('receive', {message:message ,name1: users[socket.id]})  //receive the message     
     });
 })
 
--- js/client.js
+++ js/client.js
@@ -4,9 +4,34 @@ const form = document.getElementsById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
 
-//User Joins
+const append = (message,position)=>{         //to add message
+    const messageElement = document.createElement('div'); //div(box) chatbox
+    messageElement.innerHTML=message; 
+    message.classList.add('message')  //add message
+    message.classList.add (position)
+    messageContainer.append(messageElement);
+}
 
-const name1 = prompt("Enter your name to join LetsChat")
+//Send message (Eventlistner)
+form.addEventListener('submit' , (e)=>{
+    e.preventDefault();
+    append(`You : ${message}` , right)
+    socket.emit('send' , message);
+    messageInput.value=''
+})
+
+
+//User Joins
+const name1 = prompt("Enter your name to join LetsChat") //user joined
 socket.emit('new-user-joined', name1)
 
+//listen to new user join
+socket.on('user-joined' , data=>{
+    append (`${name1} joined the chat` , 'right')
+})  //anyone who joins the chats will display
 
+
+//Receive a message
+socket.on('receive' , data=>{
+    append (`${data.name1}: ${data.message}` , 'left')
+}) //when message is received
